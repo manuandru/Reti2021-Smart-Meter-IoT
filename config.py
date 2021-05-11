a1699fc8e5a05400e6a728881a81b5edc453bd60
Manuel Andruccioli <manuandru987@gmail.com>
2021-05-11 23:32:17 +0200

    Add configuration file

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Manuel
+"""
+
+gateway_UDP_ip = 'localhost' # 192.168.1.2/24
+gateway_UDP_port = 10000
+
+number_of_clients = 4
+
+server_TCP_ip = 'localhost' # 10.10.10.2/24
+server_TCP_port = 2000
