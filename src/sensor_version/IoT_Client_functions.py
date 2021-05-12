38aa4998d729c706c3f633a6982c7f1298968304
Manuel Andruccioli <manuandru987@gmail.com>
2021-05-12 18:39:24 +0200

    Adjust date representation

--- src/sensor_version/IoT_Client_functions.py
+++ src/sensor_version/IoT_Client_functions.py
@@ -13,8 +13,8 @@ sensor = dht.DHT11(machine.Pin(4))
 
 def read_data_from_sensor():
     sensor.measure()
-    (year, month, mday, hour, minute, second, weekday, yearday) = time.localtime()
-    datetime = str(hour) + ':' + str(minute)
+    t = time.localtime()
+    datetime = '{:02d}:{:02d}:{:02d}'.format(t[3], t[4], t[5])
     temperature = sensor.temperature() # Temperature
     humidity = sensor.humidity() # Humidity
     return (datetime, temperature, humidity)
